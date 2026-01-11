c394c895d78c58030f9b0aefadd1486be8c4349c
Hritik <hritikblcm@gmail.com>
2026-01-12 01:31:15 +0530

    changes

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+export const dynamic = 'force-dynamic';
+
 const MainLayout = async ({ children }) => {
   return <div className="container mx-auto mt-24 mb-20">{children}</div>;
 };
